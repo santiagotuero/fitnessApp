d3f09b6f3d639b7ff9a6c9e511842c00cb9c8010
Santiago Tuero <santiagotuero@gmail.com>
2021-03-25 19:36:33 -0300

    minor changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import exerciseImg from '../images/exercise.png'
+import circlesImg from '../images/circles.png'
+import './styles/Card.css'
 
 class Card extends React.Component {
     render(){
-        return <h1>Card</h1>
+        const {title, description,img, leftColor, rigthColor} = this.props
+        return (
+            <div className = "card mx-auto Fitness-Card"
+                style = {{
+                    backgroundImage: `url(${circlesImg}), linear-gradient(to right, #A74CF2, #617BFB)`
+                }}
+            >
+                <div className = "card-body">
+                    <div className = "row center"> 
+                        <div className = "col-6">
+                            <img src={exerciseImg} className = "float-rigth"/>
+                        </div>
+                        <div className = "col-6 Fitness-Card-Info">
+                            <h1>{title}</h1>
+                            <p>{description}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import Card from './components/Card'
+import 'bootstrap/dist/css/bootstrap.css'
 
 const container = document.getElementById('root')
 
-ReactDom.render(<Card/>,container)
+ReactDom.render(<Card
+                    title="Technique Guides"
+                    description="Learn amazing workout and calesthenics"
+                    img="https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06"
+                    leftColor="#A74CF2"
+                    rigthColor="#617BFB"
+                />,container)
