4b32541f75f903e7b0e18f7cc61b949739c98912
Santiago Tuero <santiagotuero@gmail.com>
2021-03-26 19:48:08 -0300

    minor changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,17 +7,17 @@ class Card extends React.Component {
     render(){
         const {title, description,img, leftColor, rigthColor} = this.props
         return (
-            <div className = "card mx-auto Fitness-Card"
+            <div className="card mx-auto Fitness-Card"
                 style = {{
                     backgroundImage: `url(${circlesImg}), linear-gradient(to right, #A74CF2, #617BFB)`
                 }}
             >
-                <div className = "card-body">
-                    <div className = "row center"> 
-                        <div className = "col-6">
-                            <img src={exerciseImg} className = "float-rigth"/>
+                <div className="card-body">
+                    <div className="row center"> 
+                        <div className="col-6">
+                            <img src={exerciseImg} className="float-rigth"/>
                         </div>
-                        <div className = "col-6 Fitness-Card-Info">
+                        <div className="col-6 Fitness-Card-Info">
                             <h1>{title}</h1>
                             <p>{description}</p>
                         </div>
